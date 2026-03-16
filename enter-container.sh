9c7b819bf117b8e0f090bb8f8fb4c6f27b1c1f9f
Ian Gent <ian.gent@st-andrews.ac.uk>
2026-03-16 18:06:48 +0000

    build cleaning by Antigravity AI

--- enter-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$#" == 0 ]; then
-    docker run -it -v$(pwd):/home/Solvitaire --rm \
-    thecharlesblake/solvitaire:1.0
-else
-    docker run -it -v$(pwd):/home/Solvitaire --rm \
-    thecharlesblake/solvitaire:1.0 bash -c "$*"
-fi
